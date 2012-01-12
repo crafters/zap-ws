3cda5e77276459992bff6c5862b80b4ad3ad2bf7
Rafael Felix <felix.rafael@gmail.com>
2012-01-12 11:47:12 -0200

    Criado rails generator para gerar o initializer

---
+++ lib/generators/zap/install/install_generator.rb
@@ -0,0 +1,12 @@
+module ZapWs
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+      desc "Creates the zap config initializer on config/initializers/zap.rb."
+
+      def copy_initializer_file
+        template "initializer.rb", "config/initializers/zap.rb"
+      end
+    end
+  end
+end
---
+++ lib/generators/zap/install/templates/initializer.rb
@@ -0,0 +1,10 @@
+# Use this hook to configure you zap ws connection
+ZapWs.setup do |config|
+  # Fill your client id or client code here
+  # This should be given by zap
+  config.client_id = "YOUR CLIENT ID"
+  # Fill you password
+  # This should be given by zap
+  config.client_password = "YOUR CLIENT PASSWORD"
+end
+  
